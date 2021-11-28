1599b5c318cc3e6a67bcaf3c54ab8d67720f8910
Shresth Walia <swalia@rippling.com>
2021-11-28 15:35:23 +0530

    bug fix

--- OnlineJudge/views.py
+++ OnlineJudge/views.py
@@ -144,7 +144,7 @@ def get_results(request):
             f.write(problem[0]['testcases'])
             f.close()
         import subprocess
-        if subprocess.call(["gcc", "OnlineJudge/inputs/solution.cpp -lstdc++"]) == 0:
+        if subprocess.call(["gcc -lstdc++", "OnlineJudge/inputs/solution.cpp"]) == 0:
             subprocess.call(["./a.out <'OnlineJudge/inputs/input.txt' >'inputs/output.txt'"], shell=True)
         else:
             return JsonResponse({'status': "compilation error"})
